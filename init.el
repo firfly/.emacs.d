84745e08eeee2b85e2edb3f0b25dae761355acd0
firfly <firfly.net@qq.com>
2021-01-04 19:14:31 +0800

    add init-packages

--- init.el
+++ init.el
@@ -1,63 +1,11 @@
+(setq byte-compile-warnings '(cl-functions))
 
-  (when (>= emacs-major-version 24)
-    (require 'package)
-    (package-initialize)
-    (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
-    )
-  (require 'cl)
+(add-to-list 'load-path "~/.emacs.d/lisp/")
 
-  ;;add whatever packages you want here
-  (defvar firfly/packages '(
-                            company
-			    monokai-pro-theme
-                            hungry-delete
-			    swiper
-			    counsel
-			    smartparens
-			    js2-mode
-			    nodejs-repl
-			    exec-path-from-shell
-			    )  "Default packages")
-
-(setq package-selected-packages firfly/packages)
-      
-  (defun firfly/packages-installed-p ()
-    (loop for pkg in firfly/packages
-          when (not (package-installed-p pkg)) do (return nil)
-          finally (return t)))
-
-  (unless (firfly/packages-installed-p)
-    (message "%s" "Refreshing package database...")
-    (package-refresh-contents)
-    (dolist (pkg firfly/packages)
-      (when (not (package-installed-p pkg))
-        (package-install pkg))))
-
-;; let emacs could find the executable file
-(when (memq window-system '(mac ns))
-  (exec-path-from-shell-initialize))
-
-(require 'hungry-delete)
-(global-hungry-delete-mode)
-
-(require 'smartparens-config)
-;(add-hook 'emacs-lisp-mode-hook 'smartparens-mode)
-(smartparens-global-mode t)
-
-;; config js2-mode for js files
-(setq auto-mode-alist
-      (append
-       '(("\\.js\\'" . js2-mode))
-       auto-mode-alist))
-
-(require 'nodejs-repl)
-
-
+(require 'init-packages)
 
 ;; config for ivy (swiper) 
-(ivy-mode 1)
-(setq ivy-use-virtual-buffers t)
-(setq enable-recursive-minibuffers t)
+
 ;; enable this if you want `swiper' to use it
 ;; (setq search-default-mode #'char-fold-to-regexp)
 (global-set-key "\C-s" 'swiper)
@@ -80,7 +28,6 @@
 
 (global-set-key (kbd "<f2>") 'open-my-init-file)
 
-(global-company-mode t)
 (setq-default cursor-type 'bar)
 
 (setq make-backup-files nil)
@@ -99,7 +46,7 @@
 
 (add-hook 'emacs-lisp-mode-hook 'show-paren-mode)
 (global-hl-line-mode t)
-(load-theme 'monokai-pro t)
+
 
 (global-set-key (kbd "C-h C-f") 'find-function)
 (global-set-key (kbd "C-h C-v") 'find-variable)
